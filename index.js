dc72c3a46c0e867e46f393c8264d020321284fea
Mingfang Duan <11203duan@gmail.com>
2019-01-12 22:52:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,5 +22,5 @@ function prependKitten(name) {
   return [name,...kittens]
 }
 function removeLastKitten() {
-  return kittens.slide(0,kittens.length-1)
+  return kittens.slice(0,kittens.length-1)
 }
